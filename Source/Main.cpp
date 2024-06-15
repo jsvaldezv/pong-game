01080033c63d6ae542145a1ef5cf8b1d91c9425d
Jesús Valdez <jsvaldezv@gmail.com>
2024-06-15 00:02:47 -0600

    Create main window

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <raylib.h>
+
+const int screen_width = 1280;
+const int screen_height = 800;
 
 int main()
 {
-    std::cout << "Pong Game!\n";
-
+    std::cout << "Starting Pong Game!...\n";
+    
+    InitWindow (screen_width, screen_height, "Pong Game!");
+    SetTargetFPS (60);
+    
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        
+        EndDrawing();
+    }
+    
+    CloseWindow();
     return 0;
 }
