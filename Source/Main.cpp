45bb8d96d3dffded1f571df5f6859b8cfe702954
Jesús Valdez <jsvaldezv@gmail.com>
2024-06-14 23:29:39 -0600

    Initial commit

---
+++ Source/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "GUI/MainComponent.h"
+
+int main()
+{
+    std::cout << "Hello World from C++ Project Template!\n";
+
+    MainComponent mainComponent;
+
+    return 0;
+}
